b1f6634e65136a1ff1fdf1fa7d991941d7f67f53
Jesse McCarthy <git_commits@jessemccarthy.net>
2016-01-20 09:53:00 -0500

    Reduce to minimum input.

--- src/input.jsx
+++ src/input.jsx
@@ -1,31 +1,3 @@
-import $ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import X from 'x';
 
-import Rotator from './jsx/shared/Rotator';
-
-var View = {};
-
-/**
- * Hero
- * Renders all of the heros into the rotator
- *
- * @class
- */
-View.Hero = React.createClass({
-    displayName: 'Hero',
-
-    render: function () {
-        return (
-          <section className="hero">
-            <Rotator className="hero-promotions" hasNav hasArrows automatic>
-              <div>Rotator Content</div>
-            </Rotator>
-          </section>
-        );
-    }
-});
-
-$(function () {
-  ReactDOM.render(<View.Hero />, $('.hero-view').get(0));
-});
+<X />
