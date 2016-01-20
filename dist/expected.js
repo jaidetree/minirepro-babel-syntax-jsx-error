ba217000bb2fb8e4bafe6bfca20737e19e3a9c7c
Jay Zawrotny <jayzawrotny@gmail.com>
2016-01-20 14:51:19 -0500

    Added expected.js to repro

---
+++ dist/expected.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _X = require('jsx/shared/Rotator');
+
+var _X2 = _interopRequireDefault(_Rotator);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+<_X2 />
